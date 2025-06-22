43cb2158ad57a9ff79f1eb85304df961a44d2af7
fethulmubin <fetihul@gmail.com>
2025-06-22 15:48:52 +0300

    signin signout using nextauth 3rd parth oAuth github

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,48 @@
+import { auth, signIn, signOut } from '@/auth'
+import Link from 'next/link'
 import React from 'react'
 
-const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+const NavBar = async () => {
+    const session = await auth()
+    return (
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex items-center justify-between'>
+                <Link href='/' className='text-2xl font-bold'>
+                    <img src="/logo.png" alt="logo" />
+                </Link>
+                <div className='flex items-center gap-4 text-black'>
+                    {session && session.user ? (
+                        <>
+                            <Link href='/startup/create'>
+                                <span>Create</span>
+                            </Link>
+                            
+                            <form action={async () =>{ 
+                                "use server"
+                                await signOut({redirectTo: '/'})}}>
+                                <button type="submit">Logout</button>
+                            </form>
+                            <Link href={`/user/${session?.user.id}`}>
+                                <span>{session?.user?.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <Link href='/api/auth/signin'>
+                    
+                            <form action={async () => {
+                                "use server"
+                                await signIn('github')}}>
+                                    <button type="submit">Sign In</button>
+                            </form>
+
+                        </Link>
+                    )}
+                </div>
+
+            </nav>
+
+        </header>
+    )
 }
 
 export default NavBar
