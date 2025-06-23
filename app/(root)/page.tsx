86683b75c5773e1d18673ee0531573a2ebb31e0b
fethulmubin <fetihul@gmail.com>
2025-06-23 23:32:13 +0300

    coloer for text

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,7 +7,7 @@ const Home = ({ searchParams }: { searchParams: { query: string } }) => {
   const query = searchParams.query;
   return (
     <section className='pink_container'>
-      <h1 className='heading'>Pitch your startups<br /> <span className='text-30-semibold'>Connect to your peers</span></h1>
+      <h1 className='heading'>Pitch your startups<br /> <span className='text-30-semibold text-white'>Connect to your peers</span></h1>
       <p className="sub-heading !max-w-3xl">Join a community of like-minded entrepreneurs and take your startup to the next level.</p>
       <SearchForm query={query} />
     </section>
