dd9c315d3ea03e17d1b2facd5ff654724f52737c
fethulmubin <fetihul@gmail.com>
2025-06-23 23:31:37 +0300

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import SearchForm from '../components/SearchForm'
 
 
-const Home = () => {
+const Home = ({ searchParams }: { searchParams: { query: string } }) => {
+
+  const query = searchParams.query;
   return (
-    <div>Home</div>
+    <section className='pink_container'>
+      <h1 className='heading'>Pitch your startups<br /> <span className='text-30-semibold'>Connect to your peers</span></h1>
+      <p className="sub-heading !max-w-3xl">Join a community of like-minded entrepreneurs and take your startup to the next level.</p>
+      <SearchForm query={query} />
+    </section>
+    
   )
 }
 
---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Form from 'next/form'
+import SearchFormReset from './SearchFormReset'
+import { Search } from 'lucide-react'
+const SearchForm = ({query}: {query:String}) => {
+  
+  return (
+    <Form action='/' scroll={false} className='search-form'>
+      <input name='query' defaultValue='' type="text" placeholder="Search Startups" className="search-input" />
+        <div className='flex gap-2'>
+            {query && <SearchFormReset/>}
+            <button className='search-btn text-white' type='submit'>
+                <Search className='size-5'/>
+            </button>
+
+        </div>
+    </Form>
+  )
+}
+
+export default SearchForm
